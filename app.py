37901af4dda321aa920df62feea3e0818ae6efcc
Kartik1590 <68660756+Kartik1590@users.noreply.github.com>
2023-04-16 15:36:03 +0000

    Added css html and dynamic flask

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify
 app=Flask(__name__)
 #route is a part of url after rendering a website
+Jobs=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Benguluru',
+    'salary':'Rs. 15,00,000'
+  },
+   {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Delhi',
+    'salary':'Rs. 15,00,000'
+  },
+   {
+    'id':3,
+    'title':'Web Developer',
+    'location':'SanFransisco',
+    'salary':'Rs. 15,000,000'
+  }
+]
 @app.route('/')#empty route or homepage
 def hello_world():
-  return 'Hello World!'
+  return render_template('home.html',job=Jobs,Company_name='Honey')
+@app.route('/jobs')
+def jason():
+  return jsonify(Jobs)
 if __name__ =='__main__':
     app.run(host='0.0.0.0',debug=True) #if we dont put this then we have to use flask bash command that will be find on flask website
 
