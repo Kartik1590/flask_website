d5e8f9ec5d7ad5a1e7ee7fd270a9c0858154e95b
kartikeya <honeychoudhary9797@gmail.com>
2023-04-19 20:25:16 +0530

    username pssword removed

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine,text
-connection_string = "mysql+mysqlconnector://agizzvupuo3gwjuw1v6w:pscale_pw_DJEZE0MmLoqQETINXGpEsrlKvT8c9t5vRxNaai2CaL9@aws.connect.psdb.cloud:3306/careers"
+
 engine=create_engine(connection_string)
 
 
