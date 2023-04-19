231afc5e301f83c0f525c1c801df1a3ccbfa2074
kartikeya <honeychoudhary9797@gmail.com>
2023-04-20 02:01:35 +0530

    credentials

--- database.py
+++ database.py
@@ -1,15 +1,16 @@
 from sqlalchemy import create_engine,text
 import os
 
-connection_string=os.getenv('connection_string')
-engine=create_engine(connection_string)
+connection_string=os.environ['connection']
+print(connection_string)
 
-
+engine = create_engine(connection_string)
 def load_db():
   with engine.connect() as connection:
     result=connection.execute(text('select * from jobs'))
     result_dicts=[]
-    for row in result:
-      result_dicts.append(dict(row))
-  return result_dicts
+    for row in result.all():
+      result_dicts.append(list(row))
+    return result_dicts
 
+load_db()
