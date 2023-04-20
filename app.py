ba52d4f58447f2d7cbf583c8720739ce27b573e5
kartikeya <honeychoudhary9797@gmail.com>
2023-04-20 12:14:54 +0530

    modified and made it correct

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
 from flask import Flask,render_template,jsonify
-from database import load_db
+from flask_website.database import load_db
+import json
 app=Flask(__name__)
 #route is a part of url after rendering a website
 Jobs=load_db()
+# Jobs=[
+#   {
+#     'id':1,
+#     'title':'Data Analyst',
+#     'location':'Benguluru',
+#     'salary':'Rs. 15,00,000'
+#   },
+#    {
+#     'id':2,
+#     'title':'Data Scientist',
+#     'location':'Delhi',
+#     'salary':'Rs. 15,00,000'
+#   },
+#    {
+#     'id':3,
+#     'title':'Web Developer',
+#     'location':'SanFransisco',
+#     'salary':'Rs. 15,000,000'
+#   }
+# ]
 @app.route('/')#empty route or homepage
 def hello_world():
   return render_template('home.html',job=Jobs,Company_name='Honey')
 @app.route('/api/jobs')
 def jason():
+  
   return jsonify(Jobs)
 if __name__ =='__main__':
     app.run(host='0.0.0.0',debug=True) #if we dont put this then we have to use flask bash command that will be find on flask website
--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine,text
 import os
 
-connection_string=os.environ['connection']
+
+
+connection_string=os.environ.get('Connection_string')
 print(connection_string)
 
 engine = create_engine(connection_string)
@@ -10,7 +12,6 @@ def load_db():
     result=connection.execute(text('select * from jobs'))
     result_dicts=[]
     for row in result.all():
-      result_dicts.append(list(row))
-    return result_dicts
+      result_dicts.append(row._mapping)
+  return result_dicts
 
-load_db()
